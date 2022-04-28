9751ace7e1b3976a3452ce852e4139acd4b24caa
Kieran Megahey <kieranmegahey2@gmail.com>
2022-04-28 20:42:04 +1000

    w4

--- week 5/w5d4_12-factor_config-management-workshop-indyapyi-98746232266864b94e044c736b5cffd96c97f58d/index.js
+++
@@ -1,7 +0,0 @@
-import * as config from './config.js';
-
-console.log('dbPort', config.dbPort);
-console.log('databaseHost:- ', config.databaseHost);
-console.log('databaseName:- ', config.databaseName);
-console.log('username:- ', config.username);
-console.log('password:- ', config.password);
