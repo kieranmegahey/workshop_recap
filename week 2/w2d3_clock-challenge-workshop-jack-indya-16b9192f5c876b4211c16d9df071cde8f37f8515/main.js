9e47f80befaa74c57bebaeb0b96b60946025537d
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-05 10:06:43 +1000

    seconds hand complete

--- week 2/w2d3_clock-challenge-workshop-jack-indya-16b9192f5c876b4211c16d9df071cde8f37f8515/main.js
+++ week 2/w2d3_clock-challenge-workshop-jack-indya-16b9192f5c876b4211c16d9df071cde8f37f8515/main.js
@@ -1,3 +1,19 @@
 const hand = document.querySelector(".hand");
 
-hand.style.transform = `rotate(90deg)`; // example of how to rotate the hand
+let i = 0;
+function moveHand() {
+  hand.style.transform = `rotate(${i}deg)`;
+} // example of how to rotate the hand
+
+setInterval(() => {
+  moveHand();
+  i = i + 6;
+}, 1000);
+// // Requirements
+// You should use setInterval.
+// The hand should move round the clock every second and should complete a full revolution in 60 seconds.
+// You should adjust the rotation of the hand using the transform property as demonstrated in main.js.
+// Bonus
+// Use the JS Date object to get the real seconds.
+// Add a minute hand.
+// Add an hour hand.
