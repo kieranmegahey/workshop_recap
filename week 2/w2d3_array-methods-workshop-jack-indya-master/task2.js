c96241e871652eb5045fb515b3d6780d174bb422
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-06 20:59:02 +1000

    task 2 done

--- week 2/w2d3_array-methods-workshop-jack-indya-master/task2.js
+++ week 2/w2d3_array-methods-workshop-jack-indya-master/task2.js
@@ -1,11 +1,14 @@
 let animals = [
-    "baboon",
-    "kangaroo",
-    "rhino",
-    "frog",
-    "beaver",
-    "horse",
-    "basalisk",
-  ];
-  
-let bListAnimals = // Your 'filter' code here.
+  "baboon",
+  "kangaroo",
+  "rhino",
+  "frog",
+  "beaver",
+  "horse",
+  "basalisk",
+];
+
+let bListAnimals = animals.filter((animal) => {
+  return animal.charAt(0) === "b";
+}); // Your 'filter' code here.
+console.log(bListAnimals);
