cf281a3b9de0f514584c0e1d80866e48323d374f
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-06 21:39:33 +1000

    task 4 done

--- week 2/w2d3_array-methods-workshop-jack-indya-master/task4.js
+++ week 2/w2d3_array-methods-workshop-jack-indya-master/task4.js
@@ -1,3 +1,23 @@
 let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
 
 let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+
+// 👉 4a. Using the .every() method, check that every number in the sevenTimesTable array is a multiple of 7.
+
+const divBy7 = sevenTimesTable.every((number) => {
+  return number % 7 === 0;
+});
+console.log(divBy7);
+
+// 👉 4b. Check if every number in the seventySevenTimesTable array is a multiple of 77 using the .every() method.
+const divBy77 = sevenTimesTable.every((number) => {
+  return number % 77 === 0;
+});
+console.log(divBy77);
+// 👉 4c. Your worst fears have been confirmed. The result of step 4b showed you that there is a rogue value in your seventySevenTimesTable array. Use the .find() method to identify which number it is.
+
+const rogue = seventySevenTimesTable.find((number) => {
+  return number % 77 !== 0;
+});
+console.log(rogue);
+// 👉 4d. Now that you know which number is incorrect, use the .findIndex() method to identify the index of the number.
