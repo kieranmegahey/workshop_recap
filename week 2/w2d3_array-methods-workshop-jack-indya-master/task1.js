627618fc51eecd42072d9fcb4349db505c056121
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-06 20:54:14 +1000

    task 1 done

--- week 2/w2d3_array-methods-workshop-jack-indya-master/task1.js
+++ week 2/w2d3_array-methods-workshop-jack-indya-master/task1.js
@@ -1,5 +1,14 @@
 const cats = ["tony", "daisy", "socks", "rockie"];
 
-const capitalisedCats = cats.map((cat) => {
-  a;
-});
+// const capitalisedCats = cats.map((cat) => {
+//   return cat.toUpperCase;
+// });
+// console.log(capitalisedCats);
+
+function upper(string) {
+  return string.toUpperCase();
+}
+
+const upperCats = cats.map(upper);
+
+console.log(upperCats);
