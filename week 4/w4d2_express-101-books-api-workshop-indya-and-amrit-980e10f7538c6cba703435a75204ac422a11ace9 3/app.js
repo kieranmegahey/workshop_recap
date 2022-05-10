223679c6a715cd17fbe53d25e43d93e138208f7d
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-10 11:53:25 +1000

    workshop done

--- week 4/w4d2_express-101-books-api-workshop-indya-and-amrit-980e10f7538c6cba703435a75204ac422a11ace9 3/app.js
+++ week 4/w4d2_express-101-books-api-workshop-indya-and-amrit-980e10f7538c6cba703435a75204ac422a11ace9 3/app.js
@@ -1,9 +1,20 @@
 import express from "express";
+import books from "./books-data.js";
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the root path!" });
+});
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+app.get("/books/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const data = books.find((book) => {
+    return book.id === id;
+  });
+  res.json({ success: "true", payload: data });
 });
 
 app.listen(port, () => {
