667bdeed34066e04df243ed9a79062571f631f0c
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-17 17:44:33 +1000

    Workshop done

--- week 7/w7d2_react-usestate-pt1-workshop-room-28-alex-indya-toni-308d865202bb5dafd19299f982822df0c2f37228/src/App.js
+++ week 7/w7d2_react-usestate-pt1-workshop-room-28-alex-indya-toni-308d865202bb5dafd19299f982822df0c2f37228/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import bootcampers from "./bootcampers";
+import compliments from "./bootcampers";
+
+console.log(compliments.compliments);
 
 function App() {
+  const [bootcamperIndex, setbootcamperIndex] = useState(0);
+  const [compliment, setCompliment] = useState(0);
+  function handleClick() {
+    setbootcamperIndex(
+      Math.floor(Math.random() * bootcampers.bootcampers.length)
+    );
+    setCompliment(Math.floor(Math.random() * compliments.compliments.length));
+  }
   return (
     <div className="App">
       <h1>React useState</h1>
-      <p>BOOTCAMPER NAME</p>
-      <button>Click me for a random bootcamper!</button>
+      <p>{bootcampers.bootcampers[bootcamperIndex]}</p>
+      <p>{compliments.compliments[compliment]}</p>
+      <button onClick={handleClick}>Click me for a random bootcamper!</button>
     </div>
   );
 }
