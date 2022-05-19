6b0969a553d1052f2528e3d392d25ef49d575cf3
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-19 22:17:33 +1000

    comment

--- week 7/w7d3_immutable_updates-room-28-alex-indya-toni-effa9747f10ad2024caf372327ceb790444f0894/index.js
+++ week 7/w7d3_immutable_updates-room-28-alex-indya-toni-effa9747f10ad2024caf372327ceb790444f0894/index.js
@@ -1,19 +1,32 @@
 // Arrays:
 
 // Immutably add an item to the end of an array
-export function addToEnd(array, item) {}
+export function addToEnd(array, item) {
+  return [...array, item];
+}
 
 // Immutably add an item to the beginning of an array
-export function addToBeginning(array, item) {}
+export function addToBeginning(array, item) {
+  return [item, ...array];
+}
 
 // Immutably insert an item at a specific position/index within an array
-export function insertItem(array, item, index) {}
+export function insertItem(array, item, index) {
+  const newArr = [...array.slice(0, index), item, ...array.slice(index)];
+  return newArr; //   return [...array(index, 0, item)];
+}
 
 // Immutably replace an item at a specific position/index within an array
-export function replaceItem(array, item, index) {}
+export function replaceItem(array, item, index) {
+  const newArray = [...array.slice(0, index), item, ...array.slice(index + 1)];
+  return newArray;
+}
 
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+  // const newArray = [...array.slice(0, index), ...array.slice(index + 1)];
+  // return newArray;
+}
 
 //Objects:
 
