754aeb330304aa15aa5bba4d317eae94ed81b8e9
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-22 21:40:11 +1000

    workshop done

--- week 8/w8d1_useeffect_lesson_excercise-indyamolloy-main/src/components/App/index.js
+++ week 8/w8d1_useeffect_lesson_excercise-indyamolloy-main/src/components/App/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import List from "../List";
 import Input from "../Input";
@@ -6,8 +6,11 @@ import Input from "../Input";
 import "./App.css";
 
 function App() {
-  console.log("App rerender");
   const [toDos, setToDos] = useState(["test"]);
+  useEffect(() => {
+    document.title = `${toDos.length} things to do`;
+  }, [toDos]);
+  console.log("App rerender");
 
   function handleDelete(i) {
     console.log("%chandle delete", "color:lightblue");
