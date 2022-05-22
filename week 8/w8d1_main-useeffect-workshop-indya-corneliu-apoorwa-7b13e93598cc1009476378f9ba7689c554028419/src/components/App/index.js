48af1a1bf06455034b0a66923cd26b95e916dfbd
Kieran Megahey <kieranmegahey2@gmail.com>
2022-05-22 20:47:17 +1000

    workshop started

--- week 8/w8d1_main-useeffect-workshop-indya-corneliu-apoorwa-7b13e93598cc1009476378f9ba7689c554028419/src/components/App/index.js
+++ week 8/w8d1_main-useeffect-workshop-indya-corneliu-apoorwa-7b13e93598cc1009476378f9ba7689c554028419/src/components/App/index.js
@@ -1,24 +1,24 @@
-// import React, { useState } from "react";
-// import "./App.css";
+import React, { useState } from "react";
+import "./App.css";
 
-// import PokemonViewer from "../PokemonViewer";
+import PokemonViewer from "../PokemonViewer";
 
-// function App() {
-//   const [id, setId] = useState();
+function App() {
+  const [id, setId] = useState();
 
-//   function handleClick() {
-//     // TODO: Set id to be random number between 1 and 151
-//     setId(math.floor(math.random(1 * 150)));
-//   }
+  function handleClick() {
+    // TODO: Set id to be random number between 1 and 151
+    setId(math.floor(math.random(1 * 150)));
+  }
 
-//   return (
-//     <div className="App">
-//       {/* TODO: call handleClick when button clicked */}
-//       <button>Get Random Pokemon</button>
-//       {/* TODO: hand down id as a prop */}
-//       <PokemonViewer id={id} />
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      {/* TODO: call handleClick when button clicked */}
+      <button>Get Random Pokemon</button>
+      {/* TODO: hand down id as a prop */}
+      <PokemonViewer id={id} />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
